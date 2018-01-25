33c412941c55fa35b264436a6f14d2405a2ef78c
mbeck16@mail.csuchico.edu <mbeck16@ecc-jaguar.csuchico.edu>
2018-01-25 14:33:08 -0800

    a

---
+++ csci211/lab1/add/add.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int val1;
+int val2;
+
+int main()
+{
+    cin >> val1;
+    cin >> val2;
+
+    cout << val1 << " + " << val2 << " = " << val1 + val2 << endl;
+
+}
---
+++ csci211/lab1/print/print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int num;
+
+int main()
+{
+    cin >> num;
+
+    for(int i = 0; i < num; i++)
+    {
+        cout << i << " hello" << endl;
+
+    }
+}
