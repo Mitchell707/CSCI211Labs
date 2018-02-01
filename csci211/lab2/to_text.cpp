c7c0f0c209d605baf41b9e2371db22a1e349d85c
mbeck16@mail.csuchico.edu <mbeck16@ecc-jaguar.csuchico.edu>
2018-02-01 15:43:16 -0800

    Test

--- csci211/lab2/to_text.cpp
+++ csci211/lab2/to_text.cpp
@@ -27,34 +27,34 @@ void toText(int c)
    
     switch(c)
     {
-        case '0' : cout << "Zero" <<endl;
+        case '0' : cout << "zero" <<endl;
         break;
     
-        case '1' : cout << "One" << endl;
+        case '1' : cout << "one" << endl;
         break;
         
-        case '2' : cout << "Two" << endl;
+        case '2' : cout << "two" << endl;
         break;
         
-        case '3' : cout << "Three" << endl;
+        case '3' : cout << "three" << endl;
         break;
         
-        case '4' : cout << "Four" << endl;
+        case '4' : cout << "four" << endl;
         break;
         
-        case '5' : cout << "Five" << endl;
+        case '5' : cout << "five" << endl;
         break;
         
-        case '6' : cout << "Six" << endl;
+        case '6' : cout << "six" << endl;
         break;
 
-        case '7' : cout << "Seven" << endl;
+        case '7' : cout << "seven" << endl;
         break;
 
-        case '8' : cout << "Eight" << endl;
+        case '8' : cout << "eight" << endl;
         break;
 
-        case '9' : cout << "Nine" << endl;
+        case '9' : cout << "nine" << endl;
         break;
     
     }    
