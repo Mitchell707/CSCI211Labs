ff58b5b003cf1587168e664f5895a0ebda539961
mbeck16@mail.csuchico.edu <mbeck16@ecc-jaguar.csuchico.edu>
2018-02-03 10:21:57 -0800

    even code

---
+++ csci211/lab2/even.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int toInt(int);
+
+string input;
+
+
+int main()
+{
+    cin >> input;
+
+    for(int i = 0; i < input.size(); i++)
+    {
+        if(toInt(input[i]) % 2 != 0)
+        {  
+            //cout << toInt(input[i]) << endl;
+            //cout << "1" << endl;
+            return 1;
+        }
+
+        //input = 'a';
+    }
+    
+    return 0;
+    
+}
+
+int toInt(int c)
+{
+    //cout << c;
+   
+    switch(c)
+    {
+        case '0' : return 0;
+        break;
+    
+        case '1' : return 1;
+        break;
+        
+        case '2' : return 2;
+        break;
+        
+        case '3' : return 3;
+        break;
+        
+        case '4' : return 4;
+        break;
+        
+        case '5' : return 5;
+        break;
+        
+        case '6' : return 6;
+        break;
+
+        case '7' : return 7;
+        break;
+
+        case '8' : return 8;
+        break;
+
+        case '9' : return 9;
+        break;
+
+        //return -1;
+    
+    }    
+}
