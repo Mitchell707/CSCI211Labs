4e9e5d1aa8560911e1b54109b1879137d78d8da3
mbeck16@mail.csuchico.edu <mbeck16@ecc-jaguar.csuchico.edu>
2018-02-03 11:46:15 -0800

    code

--- csci211/lab2/even.cpp
+++ csci211/lab2/even.cpp
@@ -17,18 +17,7 @@ int main()
     
     for(int i = 0; i < input.size(); i++)
     {   
-        /*
-        if(toInt(input[i + 1] >= 0))
-        {
-            if(toInt(input[i + 1]) % 2 != 0)
-            {
-                return 1;
-            }
-
-            i++;
-        }
-        else
-        */
+   
         if(toInt(input[i]) % 2 != 0)
         {  
             //cout << toInt(input[i]) << endl;
