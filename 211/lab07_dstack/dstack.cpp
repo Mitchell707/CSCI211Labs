d16c8cd08bd63937203ad68bdb12e491332a4ab6
mbeck16@mail.csuchico.edu <mbeck16@ecc-jaguar.csuchico.edu>
2018-03-01 15:57:19 -0800

    test

--- 211/lab07_dstack/dstack.cpp
+++ 211/lab07_dstack/dstack.cpp
@@ -3,18 +3,29 @@
 
 using namespace std;
 
+Dstack::Dstack()
+{
+    m_head = NULL;
+    m_length = 0;
+}
+
 void Dstack::push(double value)
 {
-    s[top++] = value;
     
+    m_head = new Node(value, m_head); 
+    incrementSize();    
 }
 
 bool Dstack::pop(double &value)
 {
     if(!empty())
     {
-        top--;
-        value = s[top];
+        value = m_head->m_value;
+        Node *temp = m_head;
+        m_head = m_head->m_next;
+        delete temp;
+        decrementSize();
+        
         return true;
     }
     else
@@ -22,4 +33,14 @@ bool Dstack::pop(double &value)
         return false;
        
     }
+}
+
+bool Dstack::empty()
+{
+    if(size() == 0)
+    {
+        return true;
+    }
+
+    return false;
 }
--- 211/lab07_dstack/dstack.h
+++ 211/lab07_dstack/dstack.h
@@ -9,16 +9,28 @@ class Dstack
 {
     private:
         double *s;
-        int top;
+        double *top;
+        int Lsize;
+
+        class Node
+        {
+            public:
+                Node(double val, Node *next)
+                {m_value = val; m_next = next;}
+                double m_value;
+                Node *m_next;
+
+        };
+        Node *m_head;
+        int m_length;
 
     public:
         void push(double value);
         bool pop(double &value);
-        int size();
-        bool empty()
-        {   
-            return top;   
-        }
+        int size(){return Lsize;};
+        void incrementSize(){Lsize++;};
+        void decrementSize(){Lsize--;};
+        bool empty();
         Dstack();
 
 };
