e7d9462cfb0ce0f215912058f432c8588becad0a
Mitchell707 <31227154+Mitchell707@users.noreply.github.com>
2018-04-15 14:15:27 -0700

    Update triangle.h

--- 211/p6/triangle.h
+++ 211/p6/triangle.h
@@ -2,3 +2,19 @@
 //Beck, Mitchell
 //mbeck16
 
+#ifndef _TRIANGLE_H_
+#define _TRIANGLE_H_
+
+#include <iostream>
+
+using namespace std;
+
+class Triangle
+{
+  private:
+  
+  public:
+  
+};
+
+#endif
