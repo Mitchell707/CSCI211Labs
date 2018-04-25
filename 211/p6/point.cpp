265933f472e6e9265774076e0e54b0a080ef1858
Mitchell707 <31227154+Mitchell707@users.noreply.github.com>
2018-04-24 19:04:57 -0700

    Update point.cpp

--- 211/p6/point.cpp
+++ 211/p6/point.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 void Point::draw(Grid &grid)
 {
-    if(x >= 24 || y >= 60)//Makes sure the point is within the grid
+    if(x <= 24 || y <= 60)//Makes sure the point is within the grid
     {
         grid.set(x, y, c);
     }
